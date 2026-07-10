6212a24840d657ee2143919704d11e3a091dbd3c
Aryan Sinha <aryansinha0216@gmail.com>
2026-07-10 17:05:20 +0530

    Time: 0 ms (100%), Space: 13.9 MB (56.32%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long xorr=0;
+        long rightmost=0;
+        for(int i=0;i<nums.size();i++){
+            xorr=xorr^nums[i];
+            rightmost=(xorr & xorr-1) ^ xorr;
+        }
+        int b1=0,b2=0;
+        for(int i=0;i<nums.size();i++){
+            if(rightmost & nums[i])  b1=b1^nums[i];
+            else b2=b2^nums[i];
+        }
+        return {b1,b2};
+    }
+};
